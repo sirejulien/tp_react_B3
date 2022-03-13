4fc2940c79aa808d373f9eda29ac850f77ee8881
sirejulien <sire.jul@gmail.com>
2022-03-13 13:35:17 +0100

    fin css

--- mon-app/src/pages/CharacterDetail.js
+++ mon-app/src/pages/CharacterDetail.js
@@ -6,9 +6,17 @@ const CharacterDetail = () => {
 
     return (
         <div className="characterDetail">
-            <h2>{character.name}</h2>
             <img src={character.imageUrl} alt={character.name} />
+            <div className="characterDetailInfos">
+            <h2>{character.name}</h2>
             <p>This would be the character description but the API doesn't provide one...</p>
+            <h3>Appearances</h3>
+            <span><b>•Films: </b>{character.films.join(", ")}</span><br/>
+            <span><b>•Short Films: </b>{character.shortFilms.join(", ")}</span><br/>
+            <span><b>•TV Shows: </b>{character.tvShows.join(", ")}</span><br/>
+            <span><b>•Video Games: </b>{character.videoGames.join(", ")}</span><br/>
+            <span><b>•Park Attractions: </b>{character.parkAttractions.join(", ")}</span><br/>
+            </div>
         </div>
     )
 }
